d9c5232ac0c18bfb401be1c595acf916561f66af
Aaditya <aadityagupta400@gmail.com>
2023-05-21 04:50:39 +0530

    adds the sponsor pane

--- banquee/src/App.js
+++ banquee/src/App.js
@@ -1,11 +1,12 @@
 import './App.css';
 import Header from './components/header/header.js'
+import Sponsors from './components/sponsors/sponsor.js'
 
 function App() {
   return (
     <div className="App">
       <Header/>
-      
+      <Sponsors />
     </div>
   );
 }
